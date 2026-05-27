9b5c6b92433980ff025c2d09434d99666206e45a
.\.git\CaioStack <caiosalmarques7@gmail.com>
2026-05-27 18:18:48 -0300

    remove s4

--- Strings/strings4.py
+++
@@ -1,7 +0,0 @@
-# Leia uma palavra digitada pelo usuário e mostre uma nova palavra em que o último caractere seja substituído por #.
-
-palavra = input("digite uma palavra: ")
-
-nova_palavra = palavra[:-1] + "#"
-
-print(nova_palavra)
