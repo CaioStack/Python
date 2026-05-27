e2a44d97127bb69a9e99e0ed1c1e2056f9590a52
.\.git\CaioStack <caiosalmarques7@gmail.com>
2026-05-27 18:18:40 -0300

    remove s3

--- Strings/strings3.py
+++
@@ -1,7 +0,0 @@
-# Leia uma palavra digitada pelo usuário e mostre uma nova palavra em que o primeiro caractere seja substituído por 0.
-
-palavra = input("digite uma palavra: ")
-
-nova_palavra = "0" + palavra[1:]
-
-print(nova_palavra)
