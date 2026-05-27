870f51b536f4fe02bc1302dc9036202f1f359081
.\.git\CaioStack <caiosalmarques7@gmail.com>
2026-05-27 18:18:53 -0300

    remove s5

--- Strings/strings5.py
+++
@@ -1,5 +0,0 @@
-# Leia uma palavra digitada pelo usuário e mostre apenas os caracteres que estão em posições pares.
-
-palavra = input("digite uma palavra: ")
-resultado = palavra[::2]
-print(resultado)
