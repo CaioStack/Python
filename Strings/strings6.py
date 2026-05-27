f5098de938a0427813b1f94ebc42e4d7aafa4540
.\.git\CaioStack <caiosalmarques7@gmail.com>
2026-05-27 18:18:59 -0300

    remove s6

--- Strings/strings6.py
+++
@@ -1,6 +0,0 @@
-# Leia uma frase digitada pelo usuário e conte quantas vogais ela possui. O programa deve funcionar mesmo se o usuário digitar letras maiúsculas.
-
-frase = input("digite uma frase: ")
-vogais = "aeiou"
-total = sum(1 for letra in frase.lower() if letra in vogais)
-print(f"Total de vogais: {total}")
